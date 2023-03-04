f9080a23a7abcf93e75bd998760a86b4711e2716
nraj003 <126905527+nraj003@users.noreply.github.com>
2023-03-04 14:31:31 -0600

    Update spaceship.py

--- spaceship.py
+++ spaceship.py
@@ -1,16 +1,16 @@
 MAX_SPEED = 10
 MIN_SPEED = 3
 
-
 class Spaceship:
     def __init__(self, mass=20, speed=3):
         self.lives = 3
         self.speed = speed
         self.mass = mass
+        self.score = 0
 
     def update_lives(self):
         if self.lives - 1 > -1:
-            self.lives -= 1
+            self.lives-=1
 
     def update_speed(self, increase):
         if self.speed + increase <= MAX_SPEED:
@@ -22,4 +22,7 @@ class Spaceship:
         self.mass += increase
 
     def get_boost_percentage(self):
-        return (self.speed - MIN_SPEED) / MAX_SPEED
+        return (self.speed-MIN_SPEED)/MAX_SPEED
+
+    def update_score(self, increase):
+        self.score += 1
